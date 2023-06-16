49fb08fb6e6aebb08d59af906ed629ff17fd2d72
Jjuan049 <APRENDIZ@BOGDAFCGFFSP149.FormacionSena.RED>
2023-06-16 17:25:31 -0500

    Conexion MYSQL

--- Administrador/Modelo/administrador.php
+++ Administrador/Modelo/administrador.php
@@ -1,6 +1,67 @@
 <?php
-class Administrador {
+include_once('../../Conexion.php')
+class Administrador extends Conexion
+{
 
+public function __construct(){
+    $this->bd = parent::__construct();
+}
+
+//funcion para registrar los usuarios
+public function addadmi($Nombreusu,$Apellidousu,$Usuariousu,$Passwordusu,$Perfil,$Estado);
+{
+  //Crear la sentencia SQL
+  $statement = $this->bd->prepare("INSERT INTO USUARIOS(NOMBREUSU,APELLIDOUSU,USUARIO,PASSWORDU,PERFIL,ESTADO) VALUES(:NOMBREUSU,:APELLIDOUSU,:USUARIO,:PASSWORDU, :'Administrador',:'Activo')");
+
+  $statement->bindParam(':Nombreusu', $Nombreusu);
+  $statement->bindParam(':Apellidousu', $Apellidousu);
+  $statement->bindParam(':Usuariousu', $Usuariousu);
+  $statement->bindParam(':Passwordusu', $Passwordusu);
+  $statement->bindParam(':Perfil', $Perfil);
+  $statement->bindParam(':Estado', $Estado);
+  if ($statement->execute())
+  {
+
+echo " Usuario registrado ";
+header(Location: '../Pages/index.php');
+
+  }else 
+  {
+
+    echo " Usuario no registrado";
+    header(Location: '../Pages/agregar.php');
+
+  }
+
+
+
+
+
+}
+
+//Funcion para consultar todos los usuarios admin
+
+public function getadmin()
+{
+
+}
+
+//Funcion para consultar el ususario de acuerdo a su id
+public function getidad($Id)
+{
+
+}
+
+//Actualizar los datos del usuario
+public function updatead($Id,Nombreusu,$Apellidousu,$Usuariousu,$Passwordusu,$Estado);
+{
+
+}
+//Funcion para eliminar el usuario
+public function deletead($Id)
+{
+
+}
 
 }
 
--- Conexion.php
+++ Conexion.php
@@ -1,8 +1,35 @@
 <?php
 
+
 class Conexion
 {
-    
+    protected $bd;
+    private $driver = "mysql";
+    private $host = "localhost";
+    private $namebd = "notas2023php";
+    private $user = "root";
+    private $Password = "";
+
+    public function __construct()
+    {
+
+    try {
+        $bd = new PDO("{$this->driver}:host={$this->host};namebd={$this->namebd}", $this->user,$this->Password);
+
+        $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Conexion exitosa";
+
+        return $bd;
+
+        
+    } catch (PDOException $e) {
+        echo "No se puede realizar la conexion ".$e->getMessage();
+    }
+
+
+    }
+
 }
 
 ?>
