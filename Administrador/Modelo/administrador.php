da70c3961dc6608e41db7f9f6ee14010e9eb7808
Jjuan049 <APRENDIZ@BOGDAFCGFFSP149.FormacionSena.RED>
2023-06-20 17:38:22 -0500

    Estilos de registro

--- Administrador/Modelo/administrador.php
+++ Administrador/Modelo/administrador.php
@@ -33,10 +33,6 @@ header(Location: '../Pages/index.php');
 
   }
 
-
-
-
-
 }
 
 //Funcion para consultar todos los usuarios admin
@@ -44,11 +40,27 @@ header(Location: '../Pages/index.php');
 public function getadmin()
 {
 
+  $row = null;
+  $statement=$this->bd->prepare("SELECT * FROM USUARIOS WHERE Perfil='Administrador'");
+  $statement->execute();
+  while ($result->$statement->fetch()) {
+    $row[]=$result;
+  }
+  result $row;
+
 }
 
 //Funcion para consultar el ususario de acuerdo a su id
 public function getidad($Id)
 {
+
+$row=null;
+$statement=$this->bd->prepare("SELECT * FROM USUARIOS WHERE ID_USUARIOS = :Id AND Perfil ='Administrador'")
+$statement->bindParam(':Id',$Id);
+$statement ->execute();
+while ($result->statement .> fetch()) {
+eow { $result;}
+}
 
 }
 
@@ -56,11 +68,40 @@ public function getidad($Id)
 public function updatead($Id,Nombreusu,$Apellidousu,$Usuariousu,$Passwordusu,$Estado);
 {
 
+$statement=$this->bd->prepare("UPDATE USUARIOS GET NOMBREUSU=Nombreusu,APELLIDOUSU=Apellidousu,USUARIO= Usuariousu,PASSWORDU=Passwordusu,ESTADO=Estado WHERE ID_USUARIO=$Id)");
+$statement->bindparam('Id,$Id');
+statement->bindParam('NOMBREUSU,$Nombreusu');
+statement->bindParam('APELLIDOUSU,$Apellidousu');
+statement->bindParam('USUARIO,$Usuariousu');
+statement->bindParam('PASSWORDU,$Passwordusu');
+statement->bindParam('PASSWORDU,$Passwordusu');
+statement->bindParam('ESTADO,$Estado');
+
+IF ($statement->execute())
+{
+  header  ( ´location: ../Pages/index.php')
+}else
+{
+  header ('location: ../Pages/edtitar.php
+}
+
+
 }
 //Funcion para eliminar el usuario
 public function deletead($Id)
 {
 
+  $statement=$this->bd->prepare("DELETE * FROM USUARIO WHERE ID_USUARIOS=:Id");
+  $statement ->bindParam(':Id',$id);
+  if($statement->execute())
+  {
+    echo "Usuario eliminado";
+    header ('Location: ..Pages/index.php');
+  }else
+  {
+  echo " Error no se puede eliminar usuario";
+  header('Location: ..Pages/eliminar.php');
+  }
 }
 
 }
