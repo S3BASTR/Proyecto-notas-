d598f6fff89e5b6eb8c3013f8b428c6790e2336d
Jjuan049 <APRENDIZ@BOGDAFCGFFSP149.FormacionSena.RED>
2023-06-21 17:25:49 -0500

    Barra de navegacion del administrador

---
+++ Administrador/Pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pagina de inicio </title>
+    <link rel="stylesheet" href="../../Css/estilosbarran.css">
+</head>
+<body>
+
+<div class="navbar">
+    <a class="active" href="#"> Docentes </a>
+    <a class="active" href="#"> Estudiantes </a>
+    <a class="active" href="#"> Materias </a>
+    <a class="active" href="#"> Cerrar sesión </a>
+</div>
+    
+</body>
+</html>
