ea1c8684bb4fbccd30601a49fa5cc40db4621302
Sebastian acosta <APRENDIZ@BOGDAFCGFFSP144.FormacionSena.RED>
2023-06-27 14:37:51 -0500

    Paginas_Docentes_Estudiantes

---
+++ Docentes/Paginas/Editar.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+  <meta charset= "UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name= "viewport" content="width=device-width, initial-scale=1.0">
+<title>Formularios</title>
+<link rel="stylesheet" href="../../Css/AgregarDocente.css">
+</head>
+<body>
+
+ <div class= "container">
+
+ <div class="box">
+  <span class="borderLine"></span>
+
+<form>
+  <h2>Usuario</h2>
+  <div class="inputBox">
+    <input type="text" required="required">
+    <span>Nombre</span>
+    <i></i>
+  </div>
+     <div class="inputBox">
+     <input type="text" required="required">
+     <span>Apellido</span>
+     <i></i>
+  </div>
+  <div class="inputBox">
+    <input type="text" required="required">
+    <span>Documento</span>
+    <i></i>
+  </div>
+     <div class="inputBox">
+     <input type="text" required="required">
+     <span>Correo</span>
+     <i></i>
+  </div>
+  <div class="inputBox">
+    <input type="text" required="required">
+    <span>Celular</span>
+    <i></i>
+  </div>
+     <div class="inputBox">
+     <input type="text" required="required">
+     <span>Perfil</span>
+     <i></i>
+  </div>
+  <div class="inputBox">
+     <input type="text" required="required">
+     <span>Estado</span>
+     <i></i>
+  </div>
+
+    <input type="submit" value="Entrar">
